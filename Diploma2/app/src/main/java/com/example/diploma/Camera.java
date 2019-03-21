2d102f93b0dc544f5315f287be62852ce4af94cb
unknown <haiane.madoian@gmail.com>
2019-03-21 22:30:20 +0200

    Initial

---
+++ Diploma2/app/src/main/java/com/example/diploma/Camera.java
@@ -0,0 +1,4 @@
+package com.example.diploma;
+
+public class Camera {
+}
---
+++ Diploma2/app/src/main/java/com/example/diploma/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.diploma;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.support.v7.widget.SearchView;
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle abdt;
+    SearchView searchView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        searchView = (SearchView)findViewById(R.id.search_view);
+
+        dl = (DrawerLayout)findViewById(R.id.dl);
+        abdt = new ActionBarDrawerToggle(this,dl,R.string.Open,R.string.Close);
+        abdt.setDrawerIndicatorEnabled(true);
+
+        dl.addDrawerListener(abdt);
+        abdt.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        NavigationView nav_view = (NavigationView)findViewById(R.id.nav);
+        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                if(id==R.id.main)
+                {
+                    Toast.makeText(MainActivity.this, "Main", Toast.LENGTH_SHORT).show();
+                }
+                else if (id==R.id.schedule)
+                {
+                    Toast.makeText(MainActivity.this, "Schedule", Toast.LENGTH_SHORT).show();
+                }
+                else if (id == R.id.help)
+                {
+                    Toast.makeText(MainActivity.this, "Help", Toast.LENGTH_SHORT).show();
+                }
+                else if(id ==R.id.faq)
+                {
+                    Toast.makeText(MainActivity.this, "F.A.Q.", Toast.LENGTH_SHORT).show();
+                }
+
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        //MenuItem item = menu.findItem(R.id.search_view);
+        //searchView = (SearchView) MenuItemCompat.getActionView(item);
+        //searchView.setQueryHint(getString(R.string.search));
+        //searchView.setIconifiedByDefault(true);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        return abdt.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (dl.isDrawerOpen(GravityCompat.START)){
+            dl.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
